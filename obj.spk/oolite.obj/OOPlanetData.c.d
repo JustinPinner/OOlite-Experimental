47e79306b96aeb288bd0be30d7297e52f9a26128
Justin <justinpinner@gmail.com>
2013-04-26 15:44:05 +0100

    full working source tree

---
+++ obj.spk/oolite.obj/OOPlanetData.c.d
@@ -0,0 +1,14 @@
+obj.spk/oolite.obj/OOPlanetData.c.o: src/Core/OOPlanetData.c \
+ src/Core/OOPlanetData.h src/Core/OOOpenGLOnly.h \
+ /usr/include/SDL/SDL_opengl.h /usr/include/SDL/SDL_config.h \
+ /usr/include/SDL/SDL_platform.h
+
+src/Core/OOPlanetData.h:
+
+src/Core/OOOpenGLOnly.h:
+
+/usr/include/SDL/SDL_opengl.h:
+
+/usr/include/SDL/SDL_config.h:
+
+/usr/include/SDL/SDL_platform.h:
---
+++ obj.spk/oolite.obj/strlcpy.c.d
@@ -0,0 +1,4 @@
+obj.spk/oolite.obj/strlcpy.c.o: src/BSDCompat/strlcpy.c \
+ src/BSDCompat/bsd_string.h
+
+src/BSDCompat/bsd_string.h:
